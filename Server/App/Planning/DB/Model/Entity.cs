703a98c422de317fb28a39960ba64b8668735723
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-05-22 11:07:28 +0500

    techtask some changes

--- Server/App/Planning/DB/Model/Entity.cs
+++ Server/App/Planning/DB/Model/Entity.cs
@@ -20,4 +20,40 @@ namespace Planning.DB.Context
         [ColumnName("param_value")]
         public string ParamValue { get; set; }
     }
+
+    [TableName("h_user")]
+    public class UserHistory : EntityHistory
+    {
+        [ColumnName("name")]
+        public string Name { get; set; }
+        [ColumnName("description")]
+        public string Description { get; set; }
+        [ColumnName("login")]
+        public string Login { get; set; }
+        [ColumnName("password")]
+        public byte[] Password { get; set; }
+    }
+
+    public abstract class EntityHistory : IEntity
+    {
+        [PrimaryKey]
+        [ColumnName("h_id")]
+        public long HId { get; set; }
+        [ColumnName("change_date")]
+        public DateTimeOffset ChangeDate { get; set; }
+
+        [ColumnName("id")]
+        public Guid Id { get; set; }
+        [ColumnName("version_date")]
+        public DateTimeOffset VersionDate { get; set; }
+        [ColumnName("is_deleted")]
+        public bool IsDeleted { get; set; }
+    }
+
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+        bool IsDeleted { get; set; }
+        DateTimeOffset VersionDate { get; set; }
+    }
 }
