5833713ea6520a4ec20870b10c8f29cf8ea693c2
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-09-04 14:49:28 +0500

    tech task 1 changes

--- Server/App/Planning/DB/Attributes/ColumnNameAttribute.cs
+++ Server/App/Planning/DB/Attributes/ColumnNameAttribute.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿//Copyright 2021 Dmitriy Rokoth
+//Licensed under the Apache License, Version 2.0
+//
+//ref1
+using System;
 
 namespace Planning.DB.Attributes
 {
+    /// <summary>
+    /// Name of column Attribute
+    /// </summary>
     public class ColumnNameAttribute : Attribute
     {
         public string Name { get; }
